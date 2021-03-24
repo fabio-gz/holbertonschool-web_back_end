f4eac98b56b70704370565597fe0cacebe65e10f
fabio-gz <fabiogomez.silva@gmail.com>
2021-03-24 06:28:36 -0500

    [add]simple pag

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Simple pagination"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    @staticmethod
+    def index_range(page: int, page_size: int) -> tuple:
+        """start - end index"""
+        return ((page - 1) * page_size, page * page_size)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get page function"""
+        assert isinstance(page, int)
+        assert isinstance(page_size, int)
+        assert page > 0
+        assert page_size > 0
+        
+        idx = self.index_range(page, page_size)
+        baby_names = self.dataset()
+        page = baby_names[idx[0]: idx[1]]
+
+        return page
