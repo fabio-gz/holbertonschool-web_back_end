3e20797d1fcb0c7c9189e45b65c01e8f0389c9e0
fabio-gz <fabiogomez.silva@gmail.com>
2021-03-29 22:11:48 -0500

    [add]is valid

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,12 @@ def hash_password(password: str) -> bytes:
     encodedp = str.encode(password)
     hashed = bcrypt.hashpw(encodedp, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate the password"""
+    encodedp = str.encode(password)
+    if bcrypt.checkpw(encodedp, hashed_password):
+        return True
+    else:
+        return False
