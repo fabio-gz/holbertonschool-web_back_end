6cc08d832fed2d2ca10c9bf1500c53be091a8f61
fabio-gz <fabiogomez.silva@gmail.com>
2021-03-31 22:03:29 -0500

    [add]not auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,14 @@ class Auth:
     """auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """paths function"""
-        pass
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
