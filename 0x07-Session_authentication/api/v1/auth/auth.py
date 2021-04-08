a5337e385ccdd76f7abd80d26bd7a06a144f6285
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-07 21:46:43 -0500

    [add]auth

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""API authentication."""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """auth class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """paths function"""
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header"""
+        if request is None or not request.headers.get("Authorization"):
+            return None
+        else:
+            return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        pass
