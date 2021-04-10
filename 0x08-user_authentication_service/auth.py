fdc78eebc3cf8a5f8750a3f5bae952af8133c7da
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-10 10:48:17 -0500

    [Add]register

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python3
 """Hash password"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """init method"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method: Register new User"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed = _hash_password(password)
+            new_user = self._db.add_user(email, hashed)
+            return new_user
+        raise ValueError('User {} already exists'.format(user.email))
 
 
 def _hash_password(password: str) -> str:
