462ae248fbcc2440eb8c03477702052f5d9c6282
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-14 21:36:56 -0500

    [add]reset

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -70,3 +70,14 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """reset token for user"""
+        try:
+            usr = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_t = _generate_uuid()
+        self._db.update_user(usr.id, reset_token=reset_token)
+        return reset_t
