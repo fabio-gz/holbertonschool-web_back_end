635fd971ce5725d079025d924464ccca37b7d49e
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-14 21:54:55 -0500

    [add]update usr

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -81,3 +81,14 @@ class Auth:
         reset_t = _generate_uuid()
         self._db.update_user(usr.id, reset_token=reset_t)
         return reset_t
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password"""
+        try:
+            user_f = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        hashed_password = _hash_password(password)
+        self._db.update_user(
+            user_f.id, hashed_password=hashed_password, reset_token=None)
