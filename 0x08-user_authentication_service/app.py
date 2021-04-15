e3b697c25328e4b918d3bed8ff6b7b8c89ab4fd8
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-14 19:18:31 -0500

    [add]logout

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 AUTH = Auth()
@@ -44,6 +44,17 @@ def login() -> str:
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie("session_id", session)
         return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """Logout function"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if not session_id or not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
