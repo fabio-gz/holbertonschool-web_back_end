2980a29abf8853f40f7dce3412dc323c76ca1076
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-22 21:00:56 -0500

    [fix]err

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config(object):
+    """Class: Configure available languages in our app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def home():
+    """simple home page"""
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """best match with our supported languages."""
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
