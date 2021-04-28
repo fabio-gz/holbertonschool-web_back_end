740c5608ed342b16b7c6f013904361f51109a4a9
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-27 19:46:57 -0500

    [add]str int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,16 +12,24 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[bytes, str, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """generates random key"""
         rkey = str(uuid.uuid4())
         self._redis.set(rkey, data)
         return rkey
 
-    def get(self, key: str, fn: Optional[callable] = None)
-    -> Union[bytes, str, int, float]:
+    def get(self, key: str, fn: Optional[callable] = None)\
+            -> Union[bytes, str, int, float]:
         """convert the data back"""
         data = self._redis.get(key)
         if fn:
             return fn(data)
         return data
+
+    def get_str(self, data: bytes) -> str:
+        """get data in str format"""
+        return data.decode("utf-8")
+
+    def get_int(self, data: bytes) -> int:
+        """get data in int"""
+        return int.from_bytes(date, byteorder)
