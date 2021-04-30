9060fd07f20ea2fefffdc81a83d923357cf946b5
fabio-gz <fabiogomez.silva@gmail.com>
2021-04-29 19:04:25 -0500

    [add]like

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 --all glam rock bands
 SELECT band_name, (IFNULL(split,2020)-formed) AS lifespan FROM metal_bands
-WHERE style="Glam rock" ORDER BY lifespan DESC;
+WHERE style LIKE "%Glam rock%" ORDER BY lifespan DESC;
