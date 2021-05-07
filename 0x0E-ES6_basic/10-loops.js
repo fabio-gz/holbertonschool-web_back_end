b61b02f1661173d70460aaf7b2bb1e38206a6a32
fabio-gz <fabiogomez.silva@gmail.com>
2021-05-06 22:02:31 -0500

    [add]es6

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const ar = [];
+    for (const idx of array) {
+        ar.push(appendString + idx);
+    }
+    return ar;
+}
---
+++ 0x0E-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+const getBudgetObject = (income, gdp, capita) => {
+    const budget = {
+        income,
+        gdp,
+        capita,
+    };
+    return budget;
+}
+export default getBudgetObject;
